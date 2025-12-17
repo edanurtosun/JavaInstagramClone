af519631caf21fc2dfe0dc9efeed3510da702a4b
edanurtosun <eedanurtosun@hotmail.com>
2025-12-17 21:57:10 +0300

    it gives "Content is not allowed in prolog. " error

--- app/src/main/java/com/edanurtosun/javainstagramclone/MainActivity.java
+++ app/src/main/java/com/edanurtosun/javainstagramclone/MainActivity.java
@@ -1,18 +1,60 @@
 package com.edanurtosun.javainstagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.edanurtosun.javainstagramclone.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance(); // auth objesi ile sign in sign up yapabiliriz
+    }
+
+    public void signInClicked(View view){
+
+    }
+
+    public void signUpClicked(View view){
+
+        String email = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(this, "Enter email and password", Toast.LENGTH_LONG).show();
+        }else{
+            //asenkron yapida calisir
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
